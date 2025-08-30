4d9d7a74c74d2369d76489326d1254af17266878
JiajingFang <54315631+JiajingFang@users.noreply.github.com>
2025-08-30 12:56:49 +0200

    feat: hide api key (#1)
    feat:hide api key in env

--- llm_api/config.py
+++ llm_api/config.py
@@ -1,9 +1,18 @@
 # llm_api/config.py
 import os
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PORT = int(os.getenv("PORT", 8000))
+
+
+
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str = "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"
+    OPENAI_API_KEY: str = OPENAI_API_KEY
+    PORT: int = PORT
     MODEL_NAME: str = "gpt-4.1"
     TIMEOUT: int = 30
     VERSION: str = "0.1.0"
